ef4be90a6999003e09572b3812606efe945df0a9
CompuWiser <CompuWiser@outlook.com>
2018-03-17 01:34:43 +0200

    refactor: adding comments to js, local timer.js file, all node reference goes to top

--- js/app.js
+++ js/app.js
@@ -1,32 +1,31 @@
-/*
- * Create a list that holds all of your cards
- */
+let deck = document.querySelector('ul.deck');
+let resetButton = document.querySelector('div.restart');
+let modalRestart = document.querySelector('.modal-restart');
+let movesCounter = document.querySelector('span.moves');
+let timerDisplay = document.querySelector('span.timer');
+let modal = document.querySelector('.modal');
+let closeButton = document.querySelector('.close');
+let modalMoves = document.querySelector('.modal-moves');
+let modalTime = document.querySelector('.modal-time');
+let panelStars = document.querySelector('.score-panel .stars');
+let modalStars = document.querySelector('.modal-stars');
 
-const deck = document.querySelector("ul.deck");
-const resetButton = document.querySelector("div.restart");
-let movesCounter = document.querySelector("span.moves");
-let timerDisplay = document.querySelector("span.timer");
+let cardSymbols = ['repeat', 'diamond', 'paper-plane-o', 'anchor', 'bolt', 'cube', 'leaf', 'bicycle', 'bomb', 'university', 'coffee', 'eye', 'gamepad', 'gift'];
 
-
-let cardSymbols = ["repeat", "diamond", "paper-plane-o", "anchor", "bolt", "cube", "leaf", "bicycle", "bomb", "university", "coffee", "eye", "gamepad", "gift"];
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 function createCardsList(arrayOfCardSymbols) {
+
+	//get different 8 symbols each time
     shuffle(arrayOfCardSymbols);
+
+    //create 2 cards from each of the first 8 cards, to get 16 cards in total
     let cardsList = [];
     for (let j = 0; j < 2; j++) {
         for (let i = 0; i < 8; i++) {
-            let newAnchor = document.createElement("i");
-            newAnchor.setAttribute("class", "fa fa-" + arrayOfCardSymbols[i]);
-            let newElement = document.createElement("li");
-            newElement.setAttribute("class", "card");
+            let newAnchor = document.createElement('i');
+            newAnchor.setAttribute('class', 'fa fa-' + arrayOfCardSymbols[i]);
+            let newElement = document.createElement('li');
+            newElement.setAttribute('class', 'card');
             newElement.appendChild(newAnchor);
             cardsList.push(newElement);
         }
@@ -35,6 +34,7 @@ function createCardsList(arrayOfCardSymbols) {
     return shuffle(cardsList);
 }
 
+//create a random card-list, and append it to parent
 function createAndAppend(cardsList, parent) {
     let fragment = document.createDocumentFragment();
     cardsList.forEach(function(card) {
@@ -42,8 +42,6 @@ function createAndAppend(cardsList, parent) {
     });
     parent.appendChild(fragment);
 }
-
-
 
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -62,67 +60,74 @@ function shuffle(array) {
     return array;
 }
 
-const timer = new Timer();
+initializeDeckOfCards();
+
+function initializeDeckOfCards() {
+    movesCounter.innerHTML = 0;
+    createAndAppend(createCardsList(cardSymbols), deck);
+}
+
+let timer = new Timer();
 timer.addEventListener('secondsUpdated', function() {
     timerDisplay.innerHTML = timer.getTimeValues().toString();
 });
 
-startGame();
-
-resetButton.addEventListener("click", resetTheGame);
-
-document.querySelector(".modal-restart").addEventListener("click", function() {
-    modal.style.display = "none";
-    resetTheGame();
-});
+resetButton.addEventListener('click', resetTheGame);
+modalRestart.addEventListener('click', resetTheGame);
 
 function resetTheGame() {
-    deck.innerHTML = "";
+	modal.style.display = 'none';
+    deck.innerHTML = '';
     timer.stop();
-    timerDisplay.innerHTML = "00:00:00";
+    timerDisplay.innerHTML = '00:00:00';
     resetStars();
-    startGame();
+    initializeDeckOfCards();
 }
-
 
 
 let listOfRevealedCards = [];
 let numberOfMatches = 0;
 let timerStarted = false;
 
-deck.addEventListener("click", function(evt) {
+deck.addEventListener('click', function(evt) {
 
     if (!timerStarted) {
         timer.start();
         timerStarted = true;
     }
 
-
-
+    // only reveal two cards at a time
     if (listOfRevealedCards.length < 2) {
 
-        if (!evt.target.classList.contains("show")) {
-            evt.target.classList.add("show", "open", "flipInY");
+    	//only reveal un-revealed cards
+        if (!evt.target.classList.contains('show')) {
+            evt.target.classList.add('show', 'open', 'flipInY');
             listOfRevealedCards.push(evt.target)
         }
 
+        //do compare two revealed cards
         if (listOfRevealedCards.length == 2) {
             movesCounter.innerHTML++;
 
+            //set star rating based on moves count
             if (movesCounter.innerHTML == 12) {
                 setStarsOnPage(2);
             } else if (movesCounter.innerHTML == 20) {
                 setStarsOnPage(1);
             }
 
+            //if there is a match
             if (listOfRevealedCards[0].firstElementChild.classList.value === listOfRevealedCards[1].firstElementChild.classList.value) {
-                evt.target.classList.add("match");
-                listOfRevealedCards[0].classList.add("match");
+                evt.target.classList.add('match');
+                listOfRevealedCards[0].classList.add('match');
                 numberOfMatches++;
                 if (numberOfMatches == 8) matchComplete();
                 listOfRevealedCards.length = 0;
-            } else {
+            } else {	//when the two cards are not equal:
+            	//set perminent reference to last revealed cards
                 let [cardOne, cardTwo] = listOfRevealedCards;
+
+                //cards will be rehidden after 1 second, and clicks will not received during this period
                 setTimeout(function() {
                     reHide(cardOne, cardTwo);
                 }, 1000);
@@ -134,14 +139,9 @@ deck.addEventListener("click", function(evt) {
 
 function reHide(...card) {
     card.forEach(function(element) {
-        element.classList.remove("show", "open", "flipInY");
+        element.classList.remove('show', 'open', 'flipInY');
         listOfRevealedCards.length = 0;
     });
-}
-
-function startGame() {
-    movesCounter.innerHTML = 0;
-    createAndAppend(createCardsList(cardSymbols), deck);
 }
 
 function matchComplete() {
@@ -150,37 +150,34 @@ function matchComplete() {
 }
 
 
-const modal = document.querySelector('.modal');
-const closeButton = document.querySelector(".close");
-const modalMoves = document.querySelector(".modal-moves");
-const modalTime = document.querySelector(".modal-time");
+// modal that shows results, the template took from https://www.w3schools.com/howto/howto_css_modals.asp
 
-// When the user clicks the button, open the modal 
+// When the user finishes the game, open the modal 
 function displayResults() {
     modalMoves.innerHTML = movesCounter.innerHTML;
     modalTime.innerHTML = timerDisplay.innerHTML;
-    modal.style.display = "block";
+    modal.style.display = 'block';
 }
 
 // When the user clicks on <span> (x), close the modal
 closeButton.onclick = function() {
-    modal.style.display = "none";
+    modal.style.display = 'none';
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        modal.style.display = 'none';
     }
 }
 
 
-let panelStars = document.querySelector(".score-panel .stars");
-let modalStars = document.querySelector(".modal-stars");
+//format the modal content, show the results
 
+//replace star-class with other one starting from the end. styling can be applied to several elements at once
 function manipulateStarClassesFromBackToFront(numOfStarsToKeep, starClassToApply, ...elementsToApplyOn) {
     elementsToApplyOn.forEach(function(element) {
-        let starElement = element.querySelectorAll("li");
+        let starElement = element.querySelectorAll('li');
         let starElementKeys = Object.keys(starElement);
 
         for (let i = 3; i > numOfStarsToKeep; i--) {
@@ -190,22 +187,12 @@ function manipulateStarClassesFromBackToFront(numOfStarsToKeep, starClassToApply
     });
 }
 
+//reduce star rating
 function setStarsOnPage(n) {
-    manipulateStarClassesFromBackToFront(n, "fa fa-star-o", panelStars, modalStars);
+    manipulateStarClassesFromBackToFront(n, 'fa fa-star-o', panelStars, modalStars);
 }
 
+// reset stars to 3 stars
 function resetStars(){
-	manipulateStarClassesFromBackToFront(0, "fa fa-star", panelStars, modalStars);
+	manipulateStarClassesFromBackToFront(0, 'fa fa-star', panelStars, modalStars);
 }
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
