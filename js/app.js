99d6e1936e760e71b87094d5f5b95992e1793769
CompuWiser <CompuWiser@outlook.com>
2018-03-30 03:44:04 +0200

    fix: resetting the comparison array when reset-key is used

--- js/app.js
+++ js/app.js
@@ -81,6 +81,7 @@ function resetTheGame() {
     timer.stop();
     timerStarted = false;
     timerDisplay.innerHTML = '00:00:00';
+    listOfRevealedCards.length = 0;
     resetStars();
     initializeDeckOfCards();
 }
@@ -101,7 +102,7 @@ deck.addEventListener('click', function(evt) {
     if (listOfRevealedCards.length < 2) {
 
     	//only reveal un-revealed cards
-        if (!evt.target.classList.contains('show') && evt.target.computedRole == "listitem") {
+        if (!evt.target.classList.contains('show') && evt.target.classList.contains('card')) {
             evt.target.classList.add('show', 'open', 'flipInY');
             listOfRevealedCards.push(evt.target)
         }
@@ -118,7 +119,6 @@ deck.addEventListener('click', function(evt) {
             }
 
             //if there is a match
-            console.log(listOfRevealedCards);
             if (listOfRevealedCards[0].firstElementChild.classList.value === listOfRevealedCards[1].firstElementChild.classList.value) {
                 evt.target.classList.add('match');
                 listOfRevealedCards[0].classList.add('match');
